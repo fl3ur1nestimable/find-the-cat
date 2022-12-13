f49c713985d601fae368da1873cf8df852cd8632
Antoine <antoine.yebouet@telecomnancy.eu>
2022-12-13 23:36:35 +0100

    testagain

--- src/util.h
+++ src/util.h
@@ -4,7 +4,7 @@
 #include "struct_command.h"
 
 void parcoursSimple(char* directorypath,int count);
-void parcours(char* directorypath,struct_command *c,int count);
+void parcours(char* directorypath,struct_command *c/*,int count*/);
 int compare_name(struct dirent *dir, char* cheminfichier, struct_command* c);
 int compare_regex(struct dirent *dir, char* cheminfihcier, struct_command* c);
 int compare_size(char* chemin_fichier, struct_command* c);
